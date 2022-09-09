5bb9428519c8f2d840c257a7d853f0743757fd6c
Antho <AnthoneyChiocca.ac@gmail.com>
2022-09-09 16:25:51 -0400

    added whitespace for build trigger

--- src/main/java/com/crazy/scientist/crazyjavascientist/commands/CommandManager.java
+++ src/main/java/com/crazy/scientist/crazyjavascientist/commands/CommandManager.java
@@ -138,6 +138,10 @@ public class CommandManager extends ListenerAdapter {
             }
 
 
+
+
+
+
         } catch (Exception e) {
 
                 event.reply("Something went wrong, " + event.getJDA().getUserById(416342612484554752L).getName() + " will take a look into it...").queue();
